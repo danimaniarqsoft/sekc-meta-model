27773e3cb7b4675ee19495070ed5bb104baa38f6
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-03-21 17:06:57 -0600

    fist version of the meta-model

---
+++ src/main/java/mx/infotec/dads/SekcApplication.java
@@ -0,0 +1,12 @@
+package mx.infotec.dads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekcApplication.class, args);
+	}
+}
